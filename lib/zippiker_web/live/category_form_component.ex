ca38eaed4f89fb49c66a079fae8f3d988ad9fbc9
John Sseruwagi <sseruwagijohn95@gmail.com>
2025-02-21 14:35:16 +0300

    feat: add category form component

---
+++ lib/zippiker_web/live/category_form_component.ex
@@ -0,0 +1,148 @@
+defmodule ZippikerWeb.CategoryFormComponent do
+  use ZippikerWeb, :live_component
+
+  alias AshPhoenix.Form
+
+
+  @doc """
+  This funciton allows us to call this component as if it was a static component
+
+
+  ### Example:
+      - <ZippikerWeb.Categories.CategoryForm.form /> for creating a new category
+      - <ZippikerWeb.Categories.CategoryForm.form category_id={...}/> for editing an existing category
+
+
+  ### Params
+    id: unique string identifier for this components. It allows us to have this component multiple times on the same page.
+        default: UUIDv7
+    category_id: unique category identifier needed while editing this component
+  """
+  attr :id, :string, default: Ash.UUIDv7.generate()
+  attr :category_id, :string, default: nil
+
+
+  def form(assigns) do
+    ~H"""
+    <.live_component id={@id} module={__MODULE__} category_id={@category_id} />
+    """
+  end
+
+  attr :id, :string, default: Ash.UUIDv7.generate()
+  attr :category_id, :string, default: nil
+
+
+  def render(assigns) do
+    ~H"""
+    <div id={"category-form-component-#{@id}"}>
+      <%!--
+       This form must target its self so that phx-change and phx-submit events stays in it.
+       Otherwise these changes will be sent to the parent liveview
+      --%>
+      <.simple_form
+        for={@form}
+        id={"category-form-#{@category_id}"}
+        phx-submit="save"
+        phx-change="validate"
+        phx-target={@myself}
+      >
+        <h1>{gettext("New Category")}</h1>
+        <.input field={@form[:name]} label={gettext("Name")} />
+        <.input field={@form[:description]} label={gettext("Description")} type="textarea" />
+
+
+        <:actions>
+          <.button>{gettext("Submit")}</.button>
+        </:actions>
+      </.simple_form>
+    </div>
+    """
+  end
+
+  def update(assigns, socket) do
+    socket
+    |> assign(assigns)
+    |> assign_form()
+    |> ok()
+  end
+
+
+  # This callback does 2 main things:
+  # 1. Validate the submitted form params against existing form
+  # 2. Assign the validated form with its errors to the socket
+
+
+  def handle_event("validate", %{"form" => params}, socket) do
+    # 1. Validate the form
+    form = Form.validate(socket.assigns.form, params)
+
+
+    socket
+    |> assign(:form, form)
+    |> noreply()
+  end
+
+  #
+  # This function does 3 things:
+  # 1. Insert submitted form params in the database if the form is valid
+  # 2. If db insertion succeed:
+  #    2.1 It add the flash notification for the
+  #    2.2 It redirects the user to /categories listing
+
+
+  # 3. If insertion fails:
+  #    3.1 It reassign the form objects with error/ reasons for failure to display
+  #    3.2 It adds a flash notification to inform the user that it has failed.
+  #
+  def handle_event("save", %{"form" => params}, %{assigns: %{form: form}}=socket) do
+    # 1. Attempt inserting into the database
+    case Form.submit(form, params: params) do
+      {:ok, category} ->
+        socket
+        |> put_flash(:info, "Category '#{category.name}' created!")
+        |> redirect(to: ~p"/categories")
+        |> noreply()
+
+
+      # 2. Recover the error in case insertion failed.
+      {:error, form} ->
+        socket
+        |> assign(:form, form)
+        |> put_flash(:error, "Unable to create category")
+        |> noreply()
+    end
+  end
+
+
+  # Assign form to this component assigns
+  defp assign_form(socket) do
+    assign(socket, :form, get_form(socket.assigns))
+  end
+
+
+  # New category form
+  # 1. Create a changeset for Category resource
+  # 2. Inform Ash that it is for inserting new data in the data layer.
+  # 3. Converts the form into liveview form so it can be handled by simple form
+
+
+  # Prevent overriding existing form during update on changes
+  defp get_form(%{form: _form} = assigns), do: assigns
+
+
+  # Get form for creating a new category
+  defp get_form(%{category_id: nil}) do
+    Zippiker.KnowledgeBase.Category
+    |> Form.for_create(:create)
+    |> to_form()
+  end
+
+
+  # Get form for updating an existing category
+  defp get_form(%{category_id: category_id}) do
+    Zippiker.KnowledgeBase.Category
+    |> Ash.get!(category_id)
+    |> Form.for_update(:update)
+    |> to_form()
+  end
+end
--- lib/zippiker_web/live/edit_category_live.ex
+++ lib/zippiker_web/live/edit_category_live.ex
@@ -11,83 +11,19 @@ defmodule ZippikerWeb.EditCategoryLive do
   2. Assign it in the socket
   3. Assign form in the socket
   """
-  def mount(%{"category_id" => id} = _params, _session, socket) do
+  def mount(%{"category_id" => category_id} = _params, _session, socket) do
     socket
-    |> assign(:category_id, id)
-    |>assign_form()
+    |> assign(:category_id, category_id)
     |> ok()
   end
-
 
   def render(assigns) do
     ~H"""
     <%!-- Display link to take user back to category list --%>
     <.back navigate={~p"/categories"}>{gettext("Back to categories")}</.back>
-
-
-    <.simple_form
-      for={@form}
-      id={"category-form-#{@category_id}"}
-      phx-submit="save"
-      phx-change="validate"
-    >
-      <h1>{gettext("New Category")}</h1>
-      <.input field={@form[:name]} label={gettext("Name")} />
-      <.input field={@form[:description]} label={gettext("Description")} type="textarea" />
-
-
-      <:actions>
-        <.button>{gettext("Submit")}</.button>
-      </:actions>
-    </.simple_form>
+    <ZippikerWeb.CategoryFormComponent.form category_id={@category_id} />
     """
   end
 
-  @doc """
-  Validates and assign validated form to the socket
-  """
-  def handle_event("validate", %{"form" => params}, socket) do
-    form = Form.validate(socket.assigns.form, params)
 
-
-    socket
-    |> assign(:form, form)
-    |> noreply()
-  end
-
-  @doc """
-  Attempts category update or show erros explaining why it failed
-  """
-  def handle_event("save", %{"form" => params}, %{assigns: %{form: form}} = socket) do
-    case Form.submit(form, params: params) do
-      {:ok, category} ->
-        socket
-        |> put_flash(:info, "Category '#{category.name}' updated!")
-        |> noreply()
-
-
-      {:error, form} ->
-        socket
-        |> assign(:form, form)
-        |> put_flash(:error, "Unable to create category")
-        |> noreply()
-    end
-  end
-
-  @doc """
-  1. Gets category by id
-  2. Generates its AshPhoenix.Form for update action
-  3. Converts it into the phoenix form
-  4. Assign it to the socket
-  """
-  defp assign_form(%{assigns: %{category_id: id}} = socket) do
-    form =
-      Zippiker.KnowledgeBase.Category
-      |> Ash.get!(id)
-      |> Form.for_update(:update)
-      |> to_form()
-
-
-    assign(socket, :form, form)
-  end
-end
+ end
