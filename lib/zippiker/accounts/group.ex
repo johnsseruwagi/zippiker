5d384d67a6a2414698d61bc4d7485c4e8ae1b403
John Sseruwagi <sseruwagijohn95@gmail.com>
2025-04-03 15:11:27 +0300

    feat: add group table

---
+++ lib/zippiker/accounts/group.ex
@@ -0,0 +1,36 @@
+defmodule Zippiker.Accounts.Group do
+  use Ash.Resource,
+    otp_app: :zippiker,
+    domain: Zippiker.Accounts,
+    data_layer: AshPostgres.DataLayer
+
+    postgres do
+      repo Zippiker.Repo
+      table "groups"
+    end
+
+    multitenancy do
+      strategy :context
+    end
+
+    actions do
+      default_accept [:name, :description]
+      defaults [:create, :read, :update, :destroy]
+    end
+
+    changes do
+      change Zippiker.Changes.SetTenant
+    end
+
+    preparations do
+      prepare Zippiker.Preparations.SetTenant
+    end
+
+    attributes do
+      uuid_v7_primary_key :id
+      attribute :name, :string, allow_nil?: false
+      attribute :description, :string, allow_nil?: false
+
+      timestamps()
+    end
+end
